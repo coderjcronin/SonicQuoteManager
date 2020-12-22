f2c5035477d02b3d2b76e63adadf2a84d9bb8be5
CoderJCronin <coderjmc@gmail.com>
2020-12-22 14:01:57 -0500

    Vamped parts view to calc unit prices

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,9 @@ Rails.application.routes.draw do
   resources :customers
   devise_for :users
   get 'home/index'
-  get 'hello/say_hello'
-  
+
   # There is no "home" view yet. I know it's broken.
   root to: "home#index"
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
